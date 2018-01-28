02538de5c7992cb35884d7ecbef9e87a17fff677
Alexey Zinger <inline_four@yahoo.com>
2018-01-28 18:25:53 -0500

    A little more debug prints for stats_update lambda.

--- stats_update/src/stats_update.py
+++ stats_update/src/stats_update.py
@@ -1,2 +1,11 @@
+import json
+
+import boto3
+
+
 def lambda_handler(event, context):
-	print(event)
+	metadata_s3_keys = event['metadata_s3_keys']
+	stats_def = event['stats_def']
+	print(metadata_s3_keys)
+	print(stats_def)
+
