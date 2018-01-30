7fb8d55274f113a3d5d19c1f78f72e45b979b30b
Alexey Zinger <inline_four@yahoo.com>
2018-01-29 21:24:15 -0500

    First pass at stats_update lambda.

--- stats_update/src/stats_update.py
+++ stats_update/src/stats_update.py
@@ -9,3 +9,102 @@ def lambda_handler(event, context):
 	print(metadata_s3_keys)
 	print(stats_def)
 
+	s3_client = boto3.client('s3')
+
+	metadata_seq = read_metadata(metadata_s3_keys, s3_client)
+	stats_cache = {}
+	affected_stats_keys = set()
+	for metadata in metadata_seq:
+		process_metadata(metadata, stats_def, stats_cache, affected_stats_keys, s3_client)
+	for stats_key in affected_stats_keys:
+		write_stats(stats_key, stats_cache[stats_key], s3_client)
+
+
+def read_metadata(metadata_s3_keys, s3_client):
+	for metadata_s3_key in metadata_s3_keys:
+		metadata_object = s3_client.get_object(
+			Bucket='graph-wars-archive',
+			Key=metadata_s3_key
+		)
+		metadata_bytes = metadata_object['Body'].read()
+		metadata_content = metadata_bytes.decode('UTF-8')
+		metadata = json.loads(metadata_content)
+		yield metadata
+
+
+def write_stats(stats_key, stats, s3_client):
+	print('Writing stats to key {}'.format(stats_key))
+	s3_client.put_object(
+		Bucket='graph-wars-archive',
+		Key=stats_key,
+		Body=json.dumps(stats).encode('UTF-8')
+	)
+
+
+def read_stats(stats_key, s3_client):
+	try:
+		stats_object = s3_client.get_object(
+			Bucket='graph-wars-archive',
+			Key=stats_key
+		)
+		stats_bytes = stats_object['Body'].read()
+		stats_content = stats_bytes.decode('UTF-8')
+	except ex:
+		print(ex)
+		print('Will return empty list for stats.')
+		return []
+	stats = []
+	stats_header = None
+	for stats_vals in csv.reader(stats_content.split('\n')):
+		if stats_header is None:
+			stats_header = stats_vals
+		else:
+			stats_row = {}
+			for header, val in zip(stats_header, stats_vals):
+				stats_row[header] = val
+			stats.append(stats_row)
+	return stats
+
+
+def find_index_sorted(stats_list, metadata, sort_def):
+	def compare(struct1, struct2):
+		comp_list1 = []
+		comp_list2 = []
+		for sort_spec in sort_def:
+			field = sort_spec['field']
+			field1 = struct1[field]
+			field2 = struct2[field]
+			if sort_spec['direction'] == 'desc':
+				field1, field2 = field2, field1
+			comp_list1.append(field1)
+			comp_list2.append(field2)
+		if comp_list1 < comp_list2:
+			return -1
+		elif comp_list1 > comp_list2:
+			return 1
+		else:
+			return 0
+	ix = len(stats_list)
+	while ix > 0:
+		if stats_list[ix - 1]['game-key'] == metadata['game_key']:
+			return -1
+		comparison = compare(stats_list[ix - 1], metadata)
+		if comparison < 0:
+			break
+		ix -= 1
+	return ix
+
+
+def process_metadata(metadata, stats_def, stats_cache, affected_stats_keys, s3_client):
+	stats_key = stats_def['path'].format(metadata)
+	if stats_key in stats_cache:
+		stats = stats_cache[stats_key]
+	else:
+		stats = read_stats(stats_key, s3_client)
+		stats_cache[stats_key] = stats
+	insert_ix = find_index_sorted(stats, metadata, stats_def['sort'])
+	limit = stats_def['limit']
+	if 0 <= insert_ix < limit:
+		print('{} will receive game key {}'.format(stats_key, metadata['game_key']))
+		stats.insert(insert_ix, metadata)
+		affected_stats_keys.add(stats_key)
