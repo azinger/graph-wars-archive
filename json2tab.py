59f81a406d022eebb71c95b35af74d5c0caf4570
Alexey Zinger <inline_four@yahoo.com>
2018-02-10 19:05:42 -0500

    Utility for displaying json tables.

---
+++ json2tab.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+
+
+def main(args=None):
+	jsonin = sys.stdin
+	csvout = sys.stdout
+
+	content = jsonin.read()
+	data = json.loads(content)
+	if not data:
+		return
+
+	header = data[0]
+	col_lengths = list(map(len, map(str, header)))
+	for row in data[1:]:
+		for col_ix, val_len in enumerate(map(len, map(str, row))):
+			if val_len > col_lengths[col_ix]:
+				col_lengths[col_ix] = val_len
+	
+	header_formats = []
+	row_formats = []
+	for col_length in col_lengths:
+		col_length_str = str(col_length)
+		header_formats.append('{!s:^' + col_length_str + '}')
+		row_formats.append('{!s:>' + col_length_str + '}')
+	header_format = ' | '.join(header_formats)
+	row_format = ' | '.join(row_formats)
+
+	print(header_format.format(*data[0]))
+	print('-+-'.join(['-' * col_length for col_length in col_lengths]))
+	for row in data[1:]:
+		print(row_format.format(*row))
+
+
+
+if __name__ == '__main__':
+	main()
