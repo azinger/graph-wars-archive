48898daf081c8bdfe44ac659c9d1086b72380e2b
Alexey Zinger <inline_four@yahoo.com>
2018-03-04 13:40:14 -0500

    Tree-based approach to assembling nav data.

--- site_nav/src/site_nav.py
+++ site_nav/src/site_nav.py
@@ -1,9 +1,46 @@
+import itertools
 import json
 
 import boto3
 
 
 STATS_ROOT_PATH_ELEMS = ['stats', 'data']
+STATS_ROOT_PATH_LEN = len(STATS_ROOT_PATH_ELEMS)
+
+
+class PathTree(object):
+	def __init__(self, elem):
+		self._elem = elem
+		self._children = {}
+
+	def __iter__(self):
+		if self._children:
+			for child in self._children.values():
+				for child_path in child:
+					yield (self._elem,) + child_path
+		else:
+			yield (self._elem,)
+
+	def add_child_path(self, child_path):
+		if len(child_path) == 0:
+			return
+		next_elem = child_path[0]
+		remaining_elems = child_path[1:]
+		if not next_elem in self._children:
+			self._children[next_elem] = PathTree(next_elem)
+		self._children[next_elem].add_child_path(remaining_elems)
+
+	def get(self, child_path):
+		if len(child_path) == 0:
+			return self
+		next_elem = child_path[0]
+		child_tree = self._children.get(next_elem)
+		if child_tree is None:
+			return None
+		return child_tree.get(child_path[1:])
+
+	def get_child_elems(self):
+		return self._children.keys()
 
 
 def lambda_handler(event, context):
@@ -18,36 +55,48 @@ def lambda_handler(event, context):
 
 def process_stat_path(bucket_name, s3_key, s3_client):
 	path_elems = s3_key.split('/')
-	src_path_start_ix = len(STATS_ROOT_PATH_ELEMS)
-	src_path_end_ix = len(path_elems) - 1
-	for path_ix in range(src_path_start_ix, src_path_end_ix):
-		src_path_elems = path_elems[:path_ix]
-		children = []
-		load_results = True
-		marker = None
-		while load_results:
+	src_path_start_ix = STATS_ROOT_PATH_LEN
+	src_path_end_ix = len(path_elems)
+
+	stats_dir = PathTree('')
+	load_results = True
+	marker = None
+	while load_results:
+		if marker:
 			list_response = s3_client.list_objects(
 				Bucket=bucket_name,
-				Prefix='/'.join(src_path_elems),
+				Prefix='/'.join(STATS_ROOT_PATH_ELEMS),
 				Marker=marker
 			)
-			children += [elem['Key'] for elem in list_response['Contents']]
-			load_results = list_response['IsTruncated']
-			if load_results:
-				marker = children[-1]
+		else:
+			list_response = s3_client.list_objects(
+				Bucket=bucket_name,
+				Prefix='/'.join(STATS_ROOT_PATH_ELEMS)
+			)
+		for response_content in list_response['Contents']:
+			key = response_content['Key']
+			path = key.split('/')
+			stats_dir.add_child_path(path)
+		load_results = list_response['IsTruncated']
+		if load_results:
+			marker = children[-1]
+
+	for path_ix in range(src_path_start_ix, src_path_end_ix):
+		src_path_elems = path_elems[:path_ix]
+		nav_dir = stats_dir.get(src_path_elems)
+		children = nav_dir.get_child_elems()
 		write_index(src_path_elems, children, s3_client)
 
 
 def write_index(src_path_elems, children, s3_client):
+	print('Writing index for {}: {}'.format(src_path_elems, children))
 	page_lines = [
 		'<html>',
 		'<head><title>Graph Wars Archive listing</title></head>',
 		'<body>',
 		'<ul>'
 	]
-	for child in children:
-		child_path_elems = child.split('/')
-		child_path_elem = child_path_elems[len(src_path_elems)]
+	for child_path_elem in children:
 		page_lines += [
 			'<li><a href="{href}">{href}</a></li>'.format(href=child_path_elem)
 		]
@@ -57,10 +106,11 @@ def write_index(src_path_elems, children, s3_client):
 		'</html>'
 	]
 	page_content = '\n'.join(page_lines)
-	page_key = '/'.join(src_path_elems[len(STATS_ROOT_PATH_ELEMS) + 1] + 'index.html')
+	page_key = '/'.join(src_path_elems[len(STATS_ROOT_PATH_ELEMS):] + ['index.html'])
 	print('Writing index page {}'.format(page_key))
 	s3_client.put_object(
 		Bucket='graph-wars-archive-www',
 		Key=page_key,
+		ContentType='text/html; charset=utf-8',
 		Body=page_content.encode('UTF-8')
 	)
